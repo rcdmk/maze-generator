b61290e02aa75a86a7275f1d84f3a81b22261740
RCDMK <rcdmk@hotmail.com>
2020-05-05 22:09:03 +0200

    Change proportion to be based on rows and columns

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,16 @@
-const width = 400;
-const height = 400;
 const cellSize = 40;
-const cols = Math.floor(width / cellSize);
-const rows = Math.floor(height / cellSize);
+const cols = 10;
+const rows = 10;
+const width = cols * cellSize;
+const height = rows * cellSize;
+const totalCells = cols * rows;
 
-let cells = new Array(cols * rows);
+const cells = new Array(totalCells);
 
 function setup() {
     createCanvas(width, height);
 
-    for (let i = 0; i < cols * rows; i++) {
+    for (let i = 0; i < totalCells; i++) {
         cells[i] = new Cell(i % cols, Math.floor(i / rows), cellSize);
     }
 }
